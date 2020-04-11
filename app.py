e64b52ae46f836d65b33a626f7d6f30ea36abb55
Fahri MacBook <fahri-can@web.de>
2020-04-11 22:09:24 +0200

    Implement endpoints.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)  # __name__ gives the app a unique name
 stores = [
@@ -21,12 +21,21 @@ stores = [
 # POST - /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    new_request = request.get_json()
+    new_store = {
+        'name': new_request['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET - /store/<string:name>
 @app.route('/store/<string:name>')  # 'http://127.0.0.1:5000/store/any_name'
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'Something went wrong!'})
 
 # GET - /store
 @app.route('/store')
@@ -41,7 +50,10 @@ def create_item_in_store(name):
 # GET - /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'Something went wrong!'})
 
 
 app.run(port=5000)
