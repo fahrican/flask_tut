e2eeb2fcb09e866e8714125652fdf7505ffbc4c3
Fahri MacBook <fahri-can@web.de>
2020-04-13 00:34:40 +0200

    Create demo app.

---
+++ section4/code/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+# app is needed for routes
+# Api works with resources and every resource needs to be a class
+api = Api(app)
+
+
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>') # http://127.0.0.1:5000/student/Fahri
+
+app.run(port=5000)
