da765200985d3e7d2bd957ba091335c4cad5e8e4
Fahri MacBook <fahri-can@web.de>
2020-04-25 03:27:23 +0200

    Implement endpoint which delivers one specific user with the public id.

--- section4/flask_jwt_youtube/api.py
+++ section4/flask_jwt_youtube/api.py
@@ -42,9 +42,17 @@ def get_all_user():
     return jsonify({'users': output})
 
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message': 'Sorry, no user found with this user id.'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+    return jsonify({'user': user_data})
 
 
 @app.route('/user', methods=['POST'])
